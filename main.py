0c400806633c6229bcb40c14a00ea0b157e564b8
Khidirbek <khidirbek049@gmail.com>
2023-02-09 20:07:26 +0500

    functions

---
+++ main.py
@@ -0,0 +1,49 @@
+# 1
+# def cubeVolume(sideLength):
+#     volume = sideLength**3
+#     return volume
+
+
+# result1 = cubeVolume(2)
+# result2 = cubeVolume(10)
+# print(result1)
+# print(result2)
+
+# 2
+# def main():
+#     result = cubeVolume(2)
+#     print("Acube with side length 2 has volume", result)
+
+
+# def cubeVolume(sideLength):
+#     volume = sideLength**3
+#     return volume
+
+
+# main()
+
+# self check
+# 1
+# def main():
+#     result1 = cubeVolume(3)
+#     print("volume = ", result1)
+
+
+# def cubeVolume(sideLength):
+#     volume = sideLength**3
+#     return volume
+
+
+# main()
+
+# x = lambda a: a + 10
+# print(x(5))
+
+# x = lambda a, b: a * b
+# print(x(5, 6))
+
+# str1 = "hello world"
+# rev_upper = lambda string: string.upper()[::-1]
+# print(rev_upper(str1))
+# weecaercerfvs
+# awcferscf
