7cf14b6e72b3a7e548b2fb2d40ad46edd380156a
Khidirbek <khidirbek049@gmail.com>
2023-02-14 11:00:09 +0500

    14.02.2023 lesson

--- main.py
+++ main.py
@@ -45,8 +45,59 @@
 # str1 = "hello world"
 # rev_upper = lambda string: string.upper()[::-1]
 # print(rev_upper(str1))
-# weecaercerfvs
-# awcferscf
+#
 
-# scdsfvfgv
-# dsvfgv
+
+# 14.02.2023
+# 1
+
+# input_list = [1, 2, 3, 4, 5, 6, 7, 7]
+
+# output_list = []
+
+# for var in input_list:
+#     if var % 2 == 0:
+#         output_list.append(var)
+# print("output list osing for loop:", output_list)
+
+# 2
+# input_list = [1, 2, 3, 4, 5, 6, 7, 7]
+
+# list_using_comp = [var for var in input_list if var % 2 == 0]
+# print(list_using_comp)
+
+# 3
+# output_list = []
+# for var in range(1, 10):
+#     output_list.append(var**2)
+# print(output_list)
+
+
+# 4
+# list_using_comprehensive = [var**2 for var in range(1, 10)]
+# print(list_using_comprehensive)
+
+
+# 5
+# friends = []
+# friends.append("Harry")
+# friends.append("emily")
+# friends.append("bob")
+# friends.append("cari")
+# print(friends)
+
+
+# 6
+# friends = ["harry", "emily", "bob", "cari"]
+# friends.insert(1, "cindy")
+# print(friends)
+
+
+# 7
+# friends = ["harry", "emily", "bob", "cari", "cindy"]
+
+# if "cindy" in friends:
+#     print("she's a friend")
+
+# n = friends.index("emily")
+# n2 = friends.index("emily", n + 1)
